ccbd5a8ffcc5fb15e0d10cbeab9881bf3e29c18e
Sasha Levin <sasha.levin@oracle.com>
2014-12-29 09:39:01 -0500

    KEYS: close race between key lookup and freeing
    commit a3a8784454692dd72e5d5d34dcdab17b4420e74c upstream.
    
    When a key is being garbage collected, it's key->user would get put before
    the ->destroy() callback is called, where the key is removed from it's
    respective tracking structures.
    
    This leaves a key hanging in a semi-invalid state which leaves a window open
    for a different task to try an access key->user. An example is
    find_keyring_by_name() which would dereference key->user for a key that is
    in the process of being garbage collected (where key->user was freed but
    ->destroy() wasn't called yet - so it's still present in the linked list).
    
    This would cause either a panic, or corrupt memory.
    
    Fixes CVE-2014-9529.
    
    Signed-off-by: Sasha Levin <sasha.levin@oracle.com>
    Signed-off-by: David Howells <dhowells@redhat.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    
    Conflicts:
    	security/keys/gc.c

--- security/keys/gc.c
+++ security/keys/gc.c
@@ -201,11 +201,19 @@ static noinline void key_gc_unused_keys(struct list_head *keys)
 		if (test_bit(KEY_FLAG_INSTANTIATED, &key->flags))
 			atomic_dec(&key->user->nikeys);
 
+<<<<<<< HEAD
                 /* Throw away the key data if the key is instantiated */
                 if (test_bit(KEY_FLAG_INSTANTIATED, &key->flags) &&
                     !test_bit(KEY_FLAG_NEGATIVE, &key->flags) &&
                     key->type->destroy)
                         key->type->destroy(key);
+
+		key_user_put(key->user);
+=======
+		/* now throw away the key memory */
+		if (key->type->destroy)
+			key->type->destroy(key);
+>>>>>>> 5565e69... KEYS: close race between key lookup and freeing
 
 		key_user_put(key->user);
 
