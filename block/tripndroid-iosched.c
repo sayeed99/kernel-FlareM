5f9b25240c1be26e520896bdb09646ac6677135f
sayeed <sayeed.afridi2009@gmail.com>
2015-12-30 15:48:47 +0530

    fixed TripNDroid IO scheduler to work with our zenny

--- block/tripndroid-iosched.c
+++ block/tripndroid-iosched.c
@@ -193,13 +193,20 @@ static struct request *tripndroid_latter_request(struct request_queue *q, struct
 	return list_entry(rq->queuelist.next, struct request, queuelist);
 }
 
-static void *tripndroid_init_queue(struct request_queue *q)
+static int tripndroid_init_queue(struct request_queue *q, struct elevator_type *e)
 {
 	struct tripndroid_data *td;
+	struct elevator_queue *eq;
+	
+	eq = elevator_alloc(q, e);
+	if (!eq)
+		return -ENOMEM;
 
 	td = kmalloc_node(sizeof(*td), GFP_KERNEL, q->node);
 	if (!td)
-		return NULL;
+		return -ENOMEM;
+
+	eq->elevator_data = td;
 
 	INIT_LIST_HEAD(&td->fifo_list[SYNC][READ]);
 	INIT_LIST_HEAD(&td->fifo_list[SYNC][WRITE]);
@@ -212,8 +219,10 @@ static void *tripndroid_init_queue(struct request_queue *q)
 	td->fifo_expire[ASYNC][READ] = async_read_expire;
 	td->fifo_expire[ASYNC][WRITE] = async_write_expire;
 	td->fifo_batch = fifo_batch;
-
-	return td;
+	spin_lock_irq(q->queue_lock);
+	q->elevator = eq;
+	spin_unlock_irq(q->queue_lock);
+	return 0;
 }
 
 static void tripndroid_exit_queue(struct elevator_queue *e)
