18672eb315b9fb33b6676d771316e21c60ac7efc
sayeed <sayeed.afridi2009@gmail.com>
2016-01-07 16:27:51 +0530

    Added and fixed Darkness governor

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -418,6 +418,9 @@ extern struct cpufreq_governor cpufreq_gov_interactivex;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_SMARTMAX)
 extern struct cpufreq_governor cpufreq_gov_smartmax;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_smartmax)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_DARKNESS)
+extern struct cpufreq_governor cpufreq_gov_darkness;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_darkness)
 #endif
 
 /*********************************************************************
