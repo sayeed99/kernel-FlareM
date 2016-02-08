6afb631106dbebf7070eb4682a5bebf78cfabdc8
eoghan2t9 <eoghan61@gmail.com>
2016-02-07 18:28:17 -0800

    Revert "Add Brain Fuck Scheduler v0.440"
    This reverts commit 32e6a4f6710e9cd355054cc94d22fc49e0c04871.
    
    Conflicts:
    	kernel/sched/bfs.c

--- fs/proc/base.c
+++ fs/proc/base.c
@@ -362,7 +362,7 @@ out:
 static int proc_pid_schedstat(struct task_struct *task, char *buffer)
 {
 	return sprintf(buffer, "%llu %llu %lu\n",
-			(unsigned long long)tsk_seruntime(task),
+			(unsigned long long)task->se.sum_exec_runtime,
 			(unsigned long long)task->sched_info.run_delay,
 			task->sched_info.pcount);
 }
